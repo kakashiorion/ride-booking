b2b5541cbc4efcd556c3f2ff736588e8bf7daa90
Kakashi Orion <nithinkumargautam@PE-BGLR-521.local>
2022-08-10 02:19:00 +0530

    basic apollo server

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,5 @@
 import { ApolloServer } from "apollo-server-express";
-import {
-  ApolloServerPluginDrainHttpServer,
-  ApolloServerPluginLandingPageLocalDefault,
-} from "apollo-server-core";
 import express from "express";
-import http from "http";
 import { DocumentNode } from "graphql";
 import resolvers from "./graphql/resolvers/resolvers";
 import schema from "./graphql/schema";
@@ -16,22 +11,13 @@ const PORT = process.env.PORT || "4000";
 
 async function startApolloServer(typeDefs: DocumentNode, resolvers: any) {
   const app = express();
-  const httpServer = http.createServer(app);
   const server = new ApolloServer({
     typeDefs,
     resolvers,
-    csrfPrevention: true,
-    cache: "bounded",
-    plugins: [
-      ApolloServerPluginDrainHttpServer({ httpServer }),
-      ApolloServerPluginLandingPageLocalDefault({ embed: true }),
-    ],
   });
   await server.start();
   server.applyMiddleware({ app });
-  await new Promise<void>((resolve) =>
-    httpServer.listen({ port: PORT }, resolve)
-  );
+
   console.log(
     `🚀 Server ready at http://localhost:${PORT}${server.graphqlPath}`
   );
