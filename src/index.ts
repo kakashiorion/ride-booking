d2b471093b1c8572223ea196d22ad8c8a1ec8ee8
Kakashi Orion <nithinkumargautam@PE-BGLR-521.local>
2022-08-10 14:17:38 +0530

    removed dotenv

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,7 @@
 import { ApolloServer } from "apollo-server";
 import resolvers from "./graphql/resolvers/resolvers";
 import schema from "./graphql/schema";
-// import dotenv from "dotenv";
 import { DocumentNode } from "graphql";
-
-// dotenv.config();
 
 const PORT = process.env.PORT;
 
